08279513c793cfc814ad1011f01be0c57c9a56cc
Cathy Hu <cathyhu@DHCP-POOL-18-25-28-120.MIT.EDU>
2025-12-08 16:30:12 -0500

    attempting video creation function in test_top_create_video

--- sim_build/cocotb_iverilog_dump.v
+++ sim_build/cocotb_iverilog_dump.v
@@ -1,6 +1,6 @@
 module cocotb_iverilog_dump();
 initial begin
-    $dumpfile("/Users/cathyhu/fall2025/rfsoc-ntsc-decoder/sim_build/sync_detector_axis.fst");
-    $dumpvars(0, sync_detector_axis);
+    $dumpfile("/Users/cathyhu/fall2025/rfsoc-ntsc-decoder/sim_build/top.fst");
+    $dumpvars(0, top);
 end
 endmodule
